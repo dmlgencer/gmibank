12ef9c32830343f7983abc95ca801239de1cb96c
Damla Gencer <damla.45gencer@gmail.com>
2023-01-29 20:10:13 +0300

    update1

--- src/test/java/stepdefinitions/RegistrationStepDefinition.java
+++ src/test/java/stepdefinitions/RegistrationStepDefinition.java
@@ -8,7 +8,6 @@ import pages.RegisterPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 
-
 public class RegistrationStepDefinition {
 
     GmiBankHomePage gmiBankHomePage = new GmiBankHomePage();
@@ -18,25 +17,27 @@ public class RegistrationStepDefinition {
 
     @Given("user goes to {string}")
     public void user_goes_to(String string) {
-
         Driver.getDriver().get(ConfigReader.getProperty("gmi_bank_url"));
         Driver.wait(5);
 
-
-
     }
+
     @When("click the sign in symbol and register button")
     public void click_the_sign_in_symbol_and_register_button() {
         gmiBankHomePage.signInSymbol.click();
         gmiBankHomePage.registerButton.click();
 
     }
+
     @Then("verify the registration page")
     public void verify_the_registration_page() {
 
-        Assert.assertTrue(registerPage.verifyRegisterPage.isDisplayed(), "Register page can not displayed.");
+        String expectedData = "https://www.gmibank.com/account/register";
+        String actualData = Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals(actualData, expectedData, "Registration page does not verify. ");
 
     }
+
     @Then("type the ssn, firstname, lastname, address, phone number, username and email information")
     public void type_the_ssn_firstname_lastname_address_phone_number_username_and_email_information() {
 
@@ -49,24 +50,23 @@ public class RegistrationStepDefinition {
         registerPage.email.sendKeys(Faker.instance().internet().emailAddress());
 
     }
+
     @When("type the new password")
     public void type_the_new_password() {
         registerPage.firstPassword.sendKeys("Damla12345?");
 
-
-
     }
+
     @When("type the new password confirmation")
     public void type_the_new_password_confirmation() {
         registerPage.secondPassword.sendKeys("Damla12345?");
 
-
     }
+
     @Then("click the register button")
     public void click_the_register_button() {
 
         registerPage.registerButton.click();
-
 
     }
 
